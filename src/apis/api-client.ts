a5f24861d7ed68ef309e9293bbc16183a54aa80d
Felipe Oliveira <felipe.wget@gmail.com>
2023-04-26 02:56:27 +1000

    change endpoint

--- src/apis/api-client.ts
+++ src/apis/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';;
 
 export const apiClient = axios.create({
-  baseURL: '//localhost:8000/api', //import.meta.env.VITE_API_URL,
+  baseURL: '//iolproject.org/api', //import.meta.env.VITE_API_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
